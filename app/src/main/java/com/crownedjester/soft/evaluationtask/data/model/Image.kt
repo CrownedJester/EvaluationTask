a36c9884ce0401ceff58424d5e38036c8edd29f4
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2022-02-08 04:21:48 +0300

    Add field to track checked state

--- app/src/main/java/com/crownedjester/soft/evaluationtask/data/model/Image.kt
+++ app/src/main/java/com/crownedjester/soft/evaluationtask/data/model/Image.kt
@@ -1,11 +1,16 @@
 package com.crownedjester.soft.evaluationtask.data.model
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "image_table")
 data class Image(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    val uriString: String
-)
+    val uriString: String,
+) {
+
+    @Ignore
+    var isChecked: Boolean = false
+}
